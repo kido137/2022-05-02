5fc662ce13f52ec3728e8ec0331bde4c829a1ab6
CzDelta <kido137@naver.com>
2022-05-02 09:27:55 +0900

    2022 05 02

---
+++ sauce.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int MyGold = 10;
+	float MyHp = 12.5;
+	cout << MyGold + 10 << endl;
+	cout << MyGold - 10 << endl;
+	cout << MyGold * 10 << endl;
+	cout << MyGold / 10 << endl;
+
+	cout << MyGold % 2 << endl;
+
+	cout << MyHp + 7.5 << endl;
+	cout << MyHp - 7.5 << endl;
+	cout << MyHp * 7.5 << endl;
+	cout << MyHp / 7.5 << endl;
+
+	//cout << MyHp % 0.1f << endl;
+
+}
